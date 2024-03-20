465dd818600bb0aebc3140b41873911634189ec2
Aleksey Krichevskiy <boolivar@gmail.com>
2024-03-21 01:38:33 +0200

    feat: DiscoveryRequestMapper

--- jdoc-cucumber/src/main/java/org/bool/jdoc/cucumber/DiscoveryRequestMapper.java
+++ jdoc-cucumber/src/main/java/org/bool/jdoc/cucumber/DiscoveryRequestMapper.java
@@ -3,12 +3,30 @@ package org.bool.jdoc.cucumber;
 import org.bool.jdoc.core.DiscoveryRequest;
 
 import lombok.AllArgsConstructor;
+import org.junit.platform.engine.DiscoveryFilter;
 import org.junit.platform.engine.EngineDiscoveryRequest;
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 public class DiscoveryRequestMapper {
 
+    private final FeatureLoader featureLoader;
+
+    public DiscoveryRequestMapper() {
+        this(new FeatureLoader());
+    }
+
     public EngineDiscoveryRequest toCucumberDiscoveryRequest(EngineDiscoveryRequest discoveryRequest) {
-        return DiscoveryRequest.builder().build();
+        List<Path> features = featureLoader.loadFeatures(discoveryRequest);
+        return DiscoveryRequest.builder()
+                .selectors(features.stream().map(Path::toFile).map(DiscoverySelectors::selectFile).collect(Collectors.toList()))
+                .filters(List.copyOf(discoveryRequest.getFiltersByType(DiscoveryFilter.class)))
+                .params(discoveryRequest.getConfigurationParameters())
+                .listener(discoveryRequest.getDiscoveryListener())
+                .build();
     }
 }
---
+++ jdoc-cucumber/src/main/java/org/bool/jdoc/cucumber/FeatureLoader.java
@@ -0,0 +1,13 @@
+package org.bool.jdoc.cucumber;
+
+import org.junit.platform.engine.EngineDiscoveryRequest;
+
+import java.nio.file.Path;
+import java.util.List;
+
+public class FeatureLoader {
+
+    public List<Path> loadFeatures(EngineDiscoveryRequest request) {
+        return null;
+    }
+}
