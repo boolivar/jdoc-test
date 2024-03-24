7ecf32568f4dfb43212334894354ab2109a3ae92
Aleksey Krichevskiy <boolivar@gmail.com>
2024-03-24 20:29:21 +0200

    feat: Regex parser (#11)
    * feat: RegexParser and failing test for JsoupParser
    
    * feat: JsoupParser removed with jsoup dependency

---
+++ jdoc-core/src/main/java/org/bool/jdoc/core/RegexParser.java
@@ -0,0 +1,110 @@
+package org.bool.jdoc.core;
+
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+
+import java.io.BufferedReader;
+import java.io.StringReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+public class RegexParser implements JdocParser {
+
+    @AllArgsConstructor
+    private static class Tag {
+
+        final String content;
+
+        final int start;
+
+        final int end;
+
+        boolean isOpen() {
+            return content.charAt(1) != '/';
+        }
+    }
+
+    @AllArgsConstructor
+    private static class Element {
+
+        final Tag openTag;
+
+        final Tag closeTag;
+    }
+
+    private static final Pattern CODE_TAGS = Pattern.compile("<code(?: .*)?>|<\\/code>");
+
+    private final Pattern regex;
+
+    private final BiPredicate<String, String> filter;
+
+    public RegexParser(String lang) {
+        this(Pattern.compile(String.format(" lang *= *\"%s\"", lang)).asPredicate());
+    }
+
+    public RegexParser(Predicate<String> openTagFilter) {
+        this((openTag, closeTag) -> openTagFilter.test(openTag));
+    }
+
+    public RegexParser(BiPredicate<String, String> tagFilter) {
+        this(CODE_TAGS, tagFilter);
+    }
+
+    /**
+     * Extract {@literal <}code{@literal >}{@literal </}code{@literal >}
+     * blocks from javadocs.
+     *
+     * <pre><code lang="spock">
+     * def "parse javadoc and remove leading asterisks"() {
+     *   given:
+     *     def parser = new JdocParser("spock")
+     *   when:
+     *     def result = parser.parse('<code lang="spock"> * some code</code>')
+     *   then:
+     *     result == [" some code"]
+     * }
+     * </code></pre>
+     */
+    @Override
+    public List<String> parse(String content) {
+        return topLevelElements(regex.matcher(content)).stream()
+                .filter(e -> filter.test(e.openTag.content, e.closeTag.content))
+                .map(e -> content.substring(e.openTag.end, e.closeTag.start))
+                .map(this::removeAsterisks)
+                .collect(Collectors.toList());
+    }
+
+    @SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
+    private List<Element> topLevelElements(Matcher matcher) {
+        ArrayList<Element> elements = new ArrayList<>();
+        ArrayDeque<Tag> tags = new ArrayDeque<>();
+        while (matcher.find()) {
+            Tag tag = new Tag(matcher.group(), matcher.start(), matcher.end());
+            if (tag.isOpen()) {
+                tags.push(tag);
+            } else {
+                Tag openTag = tags.pop();
+                if (tags.isEmpty()) {
+                    elements.add(new Element(openTag, tag));
+                }
+            }
+        }
+        return elements;
+    }
+
+    @SneakyThrows
+    private String removeAsterisks(String content) {
+        try (BufferedReader reader = new BufferedReader(new StringReader(content))) {
+            return reader.lines()
+                .map(line -> line.replaceFirst("^ *\\*", ""))
+                .collect(Collectors.joining("\n"));
+        }
+    }
+}
--- jdoc-core/src/test/java/org/bool/jdoc/core/JdocParserTest.java
+++ jdoc-core/src/test/java/org/bool/jdoc/core/JdocParserTest.java
@@ -1,30 +1,40 @@
 package org.bool.jdoc.core;
 
-import org.jsoup.select.Evaluator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
+@TestInstance(Lifecycle.PER_CLASS)
 class JdocParserTest {
 
-    private final JdocParser parser = new JdocParser(new Evaluator.Tag("code"));
+    private final RegexParser anyLangParser = new RegexParser((open, close) -> true);
+
+    private final RegexParser testLangParser = new RegexParser("test");
+
+    List<JdocParser> allParsers() {
+        return List.of(anyLangParser, testLangParser);
+    }
 
     @Test
     void testParse() {
-        assertThat(parser.parse("""
-              <pre><code>
-                def "parse text and remove asterisks"() {
-                  given:
-                    def parser = new CodeBlockParser()
-                  when:
-                    def result = parser.parse("<code> * some code with nested<code>blocks</code></code>")
-                  then:
-                    result == [" some code with nested<code>blocks</code>"]
-                 }
-              </pre></code>
-               """))
+        assertThat(anyLangParser.parse("""
+              * <pre><code>
+              *   def "parse text and remove asterisks"() {
+              *     given:
+              *       def parser = new CodeBlockParser()
+              *     when:
+              *       def result = parser.parse("<code> * some code with nested<code>blocks</code></code>")
+              *     then:
+              *       result == [" some code with nested<code>blocks</code>"]
+              *   }
+              * </code></pre>
+              """))
             .singleElement().asString()
             .isEqualToIgnoringWhitespace("""
                  def "parse text and remove asterisks"() {
@@ -38,16 +48,55 @@ class JdocParserTest {
                  """);
     }
 
+    @Test
+    void testParseLangAttribute() {
+        assertThat(anyLangParser.parse("""
+               <pre><code lang="test">
+                 Test Code
+               </code></pre>
+               """))
+            .singleElement().asString()
+            .isEqualToIgnoringNewLines("  Test Code");
+    }
+
+    @Test
+    void testFilterLangAttribute() {
+        assertThat(testLangParser.parse("""
+               <pre><code lang="test">
+                 Test Code
+               </code></pre>
+               <pre>
+                 <code>
+                   No lang tag
+                 </code>
+                 <code lang="spock">
+                   Wrong lang tag
+                 </code>
+                 <code lang="test">
+                   One more test code
+                 </code>
+               </pre>
+               """))
+            .hasSize(2)
+            .anySatisfy(body -> assertThat(body).isEqualToIgnoringWhitespace("Test Code"))
+            .anySatisfy(body -> assertThat(body).isEqualToIgnoringWhitespace("One more test code"))
+            ;
+    }
+
     @ValueSource(strings = {"", " ", "sometext", "// comment", "/* comment */", "/** javadoc */"})
     @ParameterizedTest
     void testEmpty(String content) {
-        assertThat(parser.parse(content))
+        assertThat(anyLangParser.parse(content))
+            .describedAs("anyLangParser")
+            .isEmpty();
+        assertThat(testLangParser.parse(content))
+            .describedAs("testLangParser")
             .isEmpty();
     }
 
     @Test
     void testNested() {
-        assertThat(parser.parse("""
+        assertThat(anyLangParser.parse("""
                 <pre>
                     <code><code>one</code></code>
                     <code>two</code>
@@ -55,5 +104,14 @@ class JdocParserTest {
                 <code>three</code>
                 """))
             .contains("<code>one</code>", "two", "three");
+    }
+
+    @Test
+    void testOpenTags() {
+        assertThat(anyLangParser.parse("""
+                <pre>
+                    <code>abc<key><value><code>de</code></code>
+                </pre>
+                """)).contains("abc<key><value><code>de</code>");
     }
 }
