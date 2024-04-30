a3b121e73a16f30d767ce52595b61be49a5d8035
Aleksey Krichevskiy <boolivar@gmail.com>
2024-05-01 00:53:16 +0300

    feat: jdoc-cucumber-gradle-plugin

---
+++ jdoc-cucumber-gradle-plugin/src/main/java/org/bool/jdoc/gradle/cucumber/JdocCucumberPlugin.java
@@ -0,0 +1,11 @@
+package org.bool.jdoc.gradle.cucumber;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class JdocCucumberPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+    }
+}
