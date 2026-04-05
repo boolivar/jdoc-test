46332f739e1b53ec649ede2e341473fd90d8204c
Aleksey Krichevskiy <boolivar@gmail.com>
2026-04-06 00:46:15 +0300

    refactor: generatePackageInfo task

--- buildSrc/src/main/kotlin/jspecify-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/jspecify-conventions.gradle.kts
@@ -11,14 +11,14 @@ nullaway {
     onlyNullMarked = true
 }
 
-
 dependencies {
     errorprone("com.uber.nullaway:nullaway:0.13.1")
     errorprone("com.google.errorprone:error_prone_core:2.46.0")
-    implementation("org.jspecify:jspecify:1.0.0")
+
+    implementation("org.jspecify:jspecify")
 }
 
-tasks.withType<JavaCompile>() {
+tasks.withType<JavaCompile>().configureEach {
     options.errorprone.nullaway {
         if (name.lowercase().contains("test")) {
             disable()
@@ -29,17 +29,23 @@ tasks.withType<JavaCompile>() {
 }
 
 val generatePackageInfo = tasks.register("generatePackageInfo") {
+
     outputs.dir(layout.buildDirectory.dir("generated/package-info"))
+
     doLast {
         val template = file("$rootDir/gradle/package-info").readText()
         sourceSets.main.get().java.sourceDirectories.minus(outputs.files).forEach { baseDir ->
-            baseDir.walk().filter { p -> p.isDirectory() && with(p.list()) { any { it.endsWith(".java", true) } && none { it.equals("package-info.java", true) } } }.forEach { packageDir ->
-                val targetDir = packageDir.relativeTo(baseDir).path
-                File(File(outputs.files.singleFile, targetDir), "package-info.java").apply {
-                    parentFile.mkdirs()
-                    writeText(template.format(targetDir.replace(File.separatorChar, '.')))
+            baseDir.walk().filter { f -> f.isDirectory() }.forEach { dir ->
+                val files = dir.list()
+                if (files.any { it.endsWith(".java", ignoreCase = true) } && !files.contains("package-info.java")) {
+                    val targetDir = dir.relativeTo(baseDir).path
+                    File(File(outputs.files.singleFile, targetDir), "package-info.java").apply {
+                        parentFile.mkdirs()
+                        val packageName = targetDir.replace(File.separatorChar, '.')
+                        writeText(template.format(packageName))
+                    }
+                    logger.lifecycle("package-info.java generated for $targetDir")
                 }
-                println("package-info.java generated for $targetDir")
             }
         }
     }
