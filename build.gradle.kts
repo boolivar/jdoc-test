78ce0f16aa815c3fe50059dbecd8ac2bbe8156d9
Aleksey Krichevskiy <boolivar@gmail.com>
2026-04-09 19:24:21 +0300

    build: root kts gradle script + libs.versions.toml

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    alias(libs.plugins.versioning)
+    alias(libs.plugins.sonar)
+}
+
+scmVersion {
+    tag.prefix = ""
+    useHighestVersion = true
+    versionIncrementer("incrementMinorIfNotOnRelease", mapOf("releaseBranchPattern" to "(?>release|(?>hot)?fix)/.+"))
+    branchVersionCreator = mapOf(
+        "master" to "simple",
+        "release/.*" to "simple",
+        ".*" to "versionWithBranch"
+    )
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "boolivar_jdoc-test")
+        property("sonar.organization", "boolivar")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
+
+allprojects {
+    group = "io.github.boolivar.jdoctest"
+    version = rootProject.scmVersion.version
+}
