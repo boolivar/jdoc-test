38f6bc33a7806624b4892cc2040397e2a91cd1f5
Aleksey Krichevskiy <boolivar@gmail.com>
2026-08-26 00:27:17 +0300

    feat: JdocCucumberPlugin cucumber version 7.34.7

--- jdoc-cucumber-gradle-plugin/src/main/java/org/bool/jdoc/cucumber/gradle/JdocCucumberPlugin.java
+++ jdoc-cucumber-gradle-plugin/src/main/java/org/bool/jdoc/cucumber/gradle/JdocCucumberPlugin.java
@@ -18,7 +18,7 @@ public class JdocCucumberPlugin implements JdocTestPlugin {
 
     public static final String TEST_TASK_NAME = "jdocCucumberTest";
 
-    public static final String CUCUMBER_VERSION = "7.34.6";
+    public static final String CUCUMBER_VERSION = "7.34.7";
 
     @Override
     public void apply(Project project) {
